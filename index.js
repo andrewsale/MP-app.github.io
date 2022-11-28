886c9ca9fa3d65f8c5ffe032811950d7eda44fda
Andrew Sale <104315122+andrewsale@users.noreply.github.com>
2022-11-28 14:07:20 -0500

    update tokenizer

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ let tokenizer;
 let model;
 let modelPath = 'https://raw.githubusercontent.com/andrewsale/MP-app.github.io/main/Model_js/model.json';
 
+// For some reason the L2 regularization in tf does not 
+// connect to the L2 regularizer in tfjs
 class L2 {
-
     static className = 'L2';
-
     constructor(config) {
        return tf.regularizers.l2(config)
     }
@@ -35,6 +35,7 @@ async function loadModel() {
 // tokenize function to convert input text to list of tokenized segments
 function tokenize(text) {
     text = text.toLowerCase();
+    text = text.replace(/[!"#$%&()*+,-./:;<=>?@\[\\\]\^_`{|}~\t\n]/g, '')
     var split_text = text.split(' ');
     var tokens = [];
     split_text.forEach(element => {
@@ -78,13 +79,13 @@ function predictSpeech() {
 }
 
 
-function addSample() {
-    if (sampleSelction.value = 'NHS1'){
-        document.getElementById('userInput').value = 'The NHS has been ruined by this government. Years of Tory austerity has brought the NHS to its knees.';
-    } else if (samplesSelection.value = "rivers"){
-        document.getElementById('userInput').value = "For reasons which they could not comprehend, and in pursuance of a decision by default, on which they were never consulted, they found themselves made strangers in their own country. They found their wives unable to obtain hospital beds in childbirth, their children unable to obtain school places, their homes and neighbourhoods changed beyond recognition, their plans and prospects for the future defeated; at work they found that employers hesitated to apply to the immigrant worker the standards of discipline and competence required of the native-born worker; they began to hear, as time went by, more and more voices which told them that they were now the unwanted. On top of this, they now learn that a one-way privilege is to be established by Act of Parliament; a law which cannot, and is not intended to, operate to protect them or redress their grievances, is to be enacted to give the stranger, the disgruntled and the agent provocateur the power to pillory them for their private actions."
-    }
-}
+// function addSample() {
+//     if (sampleSelction.value = 'NHS1'){
+//         document.getElementById('userInput').value = 'The NHS has been ruined by this government. Years of Tory austerity has brought the NHS to its knees.';
+//     } else if (samplesSelection.value = "rivers"){
+//         document.getElementById('userInput').value = "For reasons which they could not comprehend, and in pursuance of a decision by default, on which they were never consulted, they found themselves made strangers in their own country. They found their wives unable to obtain hospital beds in childbirth, their children unable to obtain school places, their homes and neighbourhoods changed beyond recognition, their plans and prospects for the future defeated; at work they found that employers hesitated to apply to the immigrant worker the standards of discipline and competence required of the native-born worker; they began to hear, as time went by, more and more voices which told them that they were now the unwanted. On top of this, they now learn that a one-way privilege is to be established by Act of Parliament; a law which cannot, and is not intended to, operate to protect them or redress their grievances, is to be enacted to give the stranger, the disgruntled and the agent provocateur the power to pillory them for their private actions."
+//     }
+// }
 
 
 
